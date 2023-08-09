665eee57b46f21e5d3ef5fb120c930dd9e2250e2
Shreehari C R <shreehari000@gamil.com>
2023-08-09 22:23:52 +0530

    completed maxProduct problem

--- Week-6/maxProduct.js
+++ Week-6/maxProduct.js
@@ -1,40 +1,17 @@
-function maxProductSubarray(nums) {
-    if (nums.length === 0) {
-        return [0, []]; // No elements, so maximum product is 0
-    }
-    
-    let maxEndingHere = nums[0];
-    let minEndingHere = nums[0];
-    let maxSoFar = nums[0];
-    let maxSubarrayStartIndex = 0;
-    let maxSubarrayEndIndex = 0;
-    let currentStartIndex = 0;
+const maxProductSubarray = (elements) => {
+    let sum = -Infinity;
+    for(let i = 0; i < elements.length; i++) {
+        let subarraySum = 1;
+        for(let j = i; j < elements.length; j++) {
+            subarraySum *= elements[j];
+        };
+    if(sum < subarraySum) sum = subarraySum;
+    };
+    return sum;
+};
 
-    for (let i = 1; i < nums.length; i++) {
-        const temp = maxEndingHere;
-        maxEndingHere = Math.max(nums[i], nums[i] * maxEndingHere, nums[i] * minEndingHere);
-        minEndingHere = Math.min(nums[i], nums[i] * temp, nums[i] * minEndingHere);
-
-        if (maxEndingHere > maxSoFar) {
-            maxSoFar = maxEndingHere;
-            maxSubarrayStartIndex = currentStartIndex;
-            maxSubarrayEndIndex = i;
-        }
-
-        if (maxEndingHere <= 0) {
-            currentStartIndex = i + 1;
-        }
-    }
-
-    return [maxSoFar, nums.slice(maxSubarrayStartIndex, maxSubarrayEndIndex + 1)];
-}
-
-// Example input
 const size = 5;
 const elements = [2, 3, -2, 4, 5];
 
 // Find maximum product subarray
-const [maxProduct, maxProductSubarrayElements] = maxProductSubarray(elements);
-
-console.log("Maximum Product:", maxProduct);
-console.log("Subarray Elements:", maxProductSubarrayElements);
+console.log(maxProductSubarray(elements));;
