1694b7e6c9df625f6133ebbdf8cc00e99ebf575b
Shreehari C R <shreehari000@gamil.com>
2023-09-12 23:16:00 +0530

    created Book list react app

---
+++ Week-9/book-list/src/book/Book.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Book = ({ book }) => {
+    const {
+        title,
+        author,
+        year
+    } = book;
+  return (
+    <div>
+    <h3>{title}</h3>
+    <p>Author: {author}</p>
+    <p>Year: {year}</p>
+  </div>
+  )
+}
+
+export default Book;
---
+++ Week-9/book-list/src/book/BookList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Book from './Book';
+
+const BookList = () => {
+  const books = [
+    { title: 'Book 1', author: 'Author 1', year: 2020 },
+    { title: 'Book 2', author: 'Author 2', year: 2018 },
+    { title: 'Book 3', author: 'Author 3', year: 2022 },
+  ];
+
+  return (
+    <div>
+      {books.map((book,idx) => {
+        return(
+          <li key={idx} style={{listStyle: "none"}}>
+            <Book book={book} />
+          </li>
+        )
+      })}
+    </div>
+  )
+}
+
+export default BookList;
