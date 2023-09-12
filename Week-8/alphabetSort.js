341b09e01b01a4b373b9ef0411a0003a176c4a12
Shreehari C R <shreehari000@gamil.com>
2023-09-12 21:51:10 +0530

    week - 8 problem completed

---
+++ Week-8/alphabetSort.js
@@ -0,0 +1,11 @@
+const sortAlph = (arr) => {
+    for(let i = 0; i < arr.length - 1; i++) {
+      for(let j = i + 1; j < arr.length; j++) {
+        if(arr[i] > arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+    return arr;
+  };
+  
+  console.log(sortAlph(["banana","apple","grape","cherry"]));
+  
